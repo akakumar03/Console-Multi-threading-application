4ae721160c670440cd540cce9fa06a5e2220f8c7
akakumar03 <41191570+akakumar03@users.noreply.github.com>
2018-07-16 20:44:56 +0100

    Simple Console Multithreading examlple
    This example is for people to know how exactly a Multi-threading works.

---
+++ MultiThreadingConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+namespace MultiThreadingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadWork threadWork = new ThreadWork(); //Accessing the other class
+            Thread FirstThread = new Thread(new ThreadStart(threadWork.FirstCount));
+            Thread SecondThread = new Thread(new ThreadStart(threadWork.SecondCount));
+            FirstThread.Start();
+            SecondThread.Start();
+            Console.Read();
+        }
+       
+    }
+}
+
+
+//Thread is a light weight process which helps in increasing the performance.
---
+++ MultiThreadingConsole/ThreadWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadingConsole
+{
+    class ThreadWork
+    {
+        public void FirstCount()
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+
+                //Print the line
+                Console.WriteLine("This is the {0} time I'm printing thread 1", i);
+                //Sleep a bit
+                Thread.Sleep(100);
+
+            }
+        }
+        public void SecondCount()
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+
+                //Print the line
+                Console.WriteLine("This is the {0} time I'm printing thread 2", i);
+                //Sleep a bit
+                Thread.Sleep(100);
+
+            }
+        }
+    }
+}
